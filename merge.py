d4d1e02d48a25531e6f63d1a9b034b3022b084d1
Omkar Darekar <142470834+Omkar090804@users.noreply.github.com>
2024-10-14 15:22:57 +0530

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,11 @@
+
+def merge_arrays(arr1, arr2):
+   
+    merged_array = arr1 + arr2
+    return merged_array
+
+arr1 = [1, 2, 3]
+arr2 = [4, 5, 6]
+
+merged_array = merge_arrays(arr1, arr2)
+print("Merged Array:", merged_array)
